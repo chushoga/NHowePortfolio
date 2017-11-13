4cfef0234def3ccf53ade8c3dbde01ed474cfb46
chushoga <chushoga@gmail.com>
2017-11-13 13:06:13 +0900

    Scene Fade in and out
    Set up on the prefab of the Level Manager. Defaults to a 1second fade.

---
+++ Assets/Scripts/GUI/FadeWindow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeWindow : MonoBehaviour {
+
+	public float fadeSpeed = 1.5f; // fading speed
+	//public bool sceneStarting = true; // is the screen still fading?
+	private Image coverImage; // the black overlay image
+	private Color col = Color.black;
+
+	// Use this for initialization
+	void Start () {
+		coverImage = GetComponentInChildren<Image>(); // grab the image attached to this canvas
+		coverImage.enabled = false; // set it to disabled to start
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void FadeIn(float speed){
+		col.a = 1f;
+		coverImage.color = col;
+		coverImage.CrossFadeAlpha(0f, speed, false);
+	}
+
+	public void FadeOut(float speed){
+		col.a = 0f;
+		coverImage.color = col;
+		coverImage.CrossFadeAlpha(1f, speed, false);
+	}
+
+}
--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -2,11 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour {
 
+	private float fadeSpeed = 1f; // fading speed
+	private Canvas canvas;
+	private Transform coverImageGO; // the black overlay image
+	private Image coverImage; // the black overlay image
+
+	void Start(){
+		canvas = GetComponentInChildren<Canvas>();
+		coverImageGO = canvas.transform.Find("Image");
+		coverImage = coverImageGO.GetComponentInChildren<Image>();
+
+		// start by enableing the image: the default is hidden so we can see scene in the editor.
+		coverImage.enabled = true;
+
+		// start with fading in.
+		CrossAlphaWithCallBack(coverImage, 0f, fadeSpeed, delegate {
+			coverImage.enabled = false;
+		});
+	}
+
+	// on scene load option fade out to black.
 	public void LoadScene(string sceneName) {
-		SceneManager.LoadScene (sceneName);
+
+		Debug.Log("Start loading the scene" + sceneName);
+
+		 CrossAlphaWithCallBack(coverImage, 1f, fadeSpeed, delegate {
+			SceneManager.LoadScene(sceneName);			
+		});
+
+	}
+
+	private void CrossAlphaWithCallBack(Image img, float alpha, float duration, System.Action action){
+		StartCoroutine(CrossFadeAlphaCOR(img, alpha, duration, action));
+	}
+
+	IEnumerator CrossFadeAlphaCOR(Image img, float alpha, float duration, System.Action action){
+
+		img.enabled = true;
+
+		Color currentColor = img.color;
+
+		Color visibleColor = img.color;
+		visibleColor.a = alpha;
+
+		float counter = 0;
+
+		while(counter < duration){
+			counter += Time.deltaTime;
+			img.color = Color.Lerp(currentColor, visibleColor, counter / duration);
+			yield return null;
+		}
+		action.Invoke();
 	}
 
 }
