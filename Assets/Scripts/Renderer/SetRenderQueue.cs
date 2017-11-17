37327c380b4d2bd91fe26942432c3288201283da
chushoga <chushoga@gmail.com>
2017-11-17 15:17:24 +0900

    Another attempt at clipping the terrain

--- Assets/Scripts/Renderer/SetRenderQueue.cs
+++ Assets/Scripts/Renderer/SetRenderQueue.cs
@@ -16,6 +16,15 @@ public class SetRenderQueue : MonoBehaviour {
 	protected int[] m_queues = new int[]{3000};
 
 	protected void Awake () {
+		/*
+		Renderer renderer;
+		Terrain t = GetComponent<Terrain>();
+		if(t) {
+			renderer = t.materialTemplate;
+		} else {
+			renderer = GetComponent<Renderer>();
+		}
+		*/
 		Renderer renderer = GetComponent<Renderer>();
 		Material[] materials = renderer.materials;
 		for(int i = 0; i < materials.Length && i < m_queues.Length; i++) {
